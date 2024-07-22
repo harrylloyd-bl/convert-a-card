99726a60c93a483c606d67280734518b626c9e6d
harrylloyd-bl <harry.lloyd@bl.uk>
2024-07-22 16:34:05 +0100

    add docstrings and type hints

--- src/utils/streamlit_utils.py
+++ src/utils/streamlit_utils.py
@@ -1,11 +1,13 @@
 import re
+from typing import Dict, List, Union
 
 import numpy as np
 import pandas as pd
 import streamlit as st
+from pymarc import Record
 
 
-def get_pub_date(record):
+def get_pub_date(record: Record) -> int:
     # Look for a date in first 260$c, if absent include in search anyway
     f260 = record.get_fields("260")
     c_subfields = []
@@ -22,7 +24,27 @@ def get_pub_date(record):
         return -9999
 
 
-def pretty_filter_option(option):
+def get_008_date(record: Record) -> str:
+    """
+    Slice the date from the correct chunk of the 008 field based on the 6th character ("Type of Date")
+    @param record: pymarc.Record
+    @return: str
+    """
+    f008 = record.get_fields("008")[0].data
+    date_type = f008[6]
+    map = {
+        's': slice(7, 11), 'r': slice(7, 11), 'q': slice(7, 15), 'n': slice(0, 0), 'm': slice(7, 15),
+        'c': slice(7, 15), 't': slice(7, 11), 'd': slice(7, 15), 'e': slice(7, 11), 'i': slice(7, 15)
+    }
+    return f008[map[date_type]]
+
+
+def pretty_filter_option(option: str) -> str:
+    """
+    Convert backend filter string to frontend label
+    @param option: str
+    @return: str
+    """
     display_dict = {
         "num_subject_access": "Number of subject access fields",
         "num_rda": "Number of RDA fields",
@@ -114,7 +136,7 @@ def sort_sf_rpt_fields_idx(index):
         return index
 
 
-def simplify_6xx(df):
+def simplify_6xx(df: pd.DataFrame) -> pd.DataFrame:
     """
     Pandas magic
     Arbitrarily assinging repeat_id vals to repeat fields in records means common field values are not matched to each other
@@ -156,31 +178,36 @@ def simplify_6xx(df):
     return tidy_df
 
 
-def filter_on_generic_fields(df, fields, terms, include_recs_without_field):
+def filter_on_generic_fields(
+        marc_df: pd.DataFrame,
+        fields: Union[None, List[str]],
+        terms: Union[None, List[str]],
+        include_recs_without_field: bool
+) -> pd.DataFrame:
     """
     Input the marc_table_df with all records
     sum all repeat columns and transpose to make searching easier
     Search each column in fields for the corresponding term in terms
     Return a df with only records that match the search terms
-    @param df:
+    @param marc_df:
     @param fields:
     @param terms:
     @return:
     """
     if not fields or not terms:
-        return df
+        return marc_df
 
-    t_df = df.groupby(level=0).sum().T
+    t_df = marc_df.groupby(level=0).sum().T
     terms = [x.strip() for x in terms]
     filter_df = pd.concat([t_df[field].str.contains(term) for field, term in zip(fields, terms)], axis=1)
     if include_recs_without_field:
         df_filter = filter_df.all(axis=1)
     else:
         df_filter = filter_df.where(lambda x: ~x.isna(), False).all(axis=1)
-    return df.T[df_filter].T
+    return marc_df.T[df_filter].T
 
 
-def gen_gmap(col):
+def gen_gmap(col: pd.Series) -> pd.Series:
     counts = col.value_counts()
     to_highlight = counts[counts > 1]
     no_highlight = counts[counts == 1]
@@ -191,42 +218,65 @@ def gen_gmap(col):
     return col.map(mapping, na_action='ignore')
 
 
-def style_marc_df(df, highlight_common_vals, existing_selected_match):
-    styled_df = df.style
+def style_marc_df(marc_df: pd.DataFrame, highlight_common_vals: bool, existing_selected_match: int) -> pd.DataFrame:
+    styled_df = marc_df.style
     if highlight_common_vals:
-        gmap = df.apply(gen_gmap, axis=1)
+        gmap = marc_df.apply(gen_gmap, axis=1)
         gmap[gmap.isna()] = -10
         gmap[1::3] += 0.05
         gmap[2::3] += 0.1
         styled_df = styled_df.background_gradient(gmap=gmap, vmin=0, vmax=1, axis=None)
-    if existing_selected_match and existing_selected_match in df.columns:
+    if existing_selected_match and existing_selected_match in marc_df.columns:
         styled_df = styled_df.highlight_between(subset=[existing_selected_match], color="#2FD033A0")
     return styled_df
 
 
-def create_filter_columns(df, lang_dict, search_au):
-    df["has_title"] = df["record"].apply(lambda x: bool(x.get_fields("245")))
-    df["has_author"] = df["record"].apply(lambda x: bool(x.get_fields("100", "110", "111", "130")))
+def create_filter_columns(record_df: pd.DataFrame, lang_dict: Dict[str, str], search_au: str) -> pd.DataFrame:
+    """
+    Add cols to the dataframe of records (not the MARC df) that allow filtering and searching on certain params
+    Cols params added are:
+    has_title: has a 245 field
+    has_author: has a 100 field
+    cataloguing language: 040$b
+    num_subject_access: # subject access fields (see field IDs below)
+    num_rda: # RDA fields (see field IDs below)
+    num_linked: # 880 fields
+    has_phys_desc: has a 300 field
+    good_encoding_level: 17th char in leader not one of 3, 5, 7
+    record_length: total fields in record
+    publication_date:
+    @param record_df: pd.DataFrame
+    @param lang_dict: Dict[str,str]
+    @param search_au: str
+    @return: pd.DataFrame
+    """
+    # title/author
+    record_df["has_title"] = record_df["record"].apply(lambda x: bool(x.get_fields("245")))
+    record_df["has_author"] = record_df["record"].apply(lambda x: bool(x.get_fields("100", "110", "111", "130")))
     au_exists = bool(search_au)
-    df = df.query("has_title == True and (has_author == True or not @au_exists)")
-    re_040b = re.compile(r"\$b[a-z]+\$")
-    df["language_040$b"] = df["record"].apply(
-        lambda x: re_040b.search(x.get_fields("040")[0].__str__()).group())
-    df["language"] = df["language_040$b"].str[2:-1].map(lang_dict["codes"])
+    record_df = record_df.query("has_title == True and (has_author == True or not @au_exists)")
 
+    # lang
+    re_040b = re.compile(r"\$b[a-z]+\$")
+    record_df["language_040$b"] = record_df["record"].apply(
+        lambda x: re_040b.search(x.get_fields("040")[0].__str__()).group())
+    record_df["language"] = record_df["language_040$b"].str[2:-1].map(lang_dict["codes"])
+
+    # subject access/RDA
     subject_access_fields = ["600", "610", "611", "630", "647", "648", "650", "651", "653", "654", "655", "656", "657",
                              "658", "662", "688"]
     rda_fields = ["264", "336", "337", "338", "344", "345", "346", "347"]
-    df["num_subject_access"] = df["record"].apply(lambda x: len(x.get_fields(*subject_access_fields)))
-    df["num_rda"] = df["record"].apply(lambda x: len(x.get_fields(*rda_fields)))
+    record_df["num_subject_access"] = record_df["record"].apply(lambda x: len(x.get_fields(*subject_access_fields)))
+    record_df["num_rda"] = record_df["record"].apply(lambda x: len(x.get_fields(*rda_fields)))
 
-    df["num_linked"] = df["record"].apply(lambda x: len(x.get_fields("880")))
-    df["has_phys_desc"] = df["record"].apply(lambda x: bool(x.get_fields("300")))
-    df["good_encoding_level"] = df["record"].apply(lambda x: x.leader[17] not in [3, 5, 7])
-    df["record_length"] = df["record"].apply(lambda x: len(x.get_fields()))
-    df["publication_date"] = df["record"].apply(lambda x: get_pub_date(x))
+    # other
+    record_df["num_linked"] = record_df["record"].apply(lambda x: len(x.get_fields("880")))
+    record_df["has_phys_desc"] = record_df["record"].apply(lambda x: bool(x.get_fields("300")))
+    record_df["good_encoding_level"] = record_df["record"].apply(lambda x: x.leader[17] not in [3, 5, 7])
+    record_df["record_length"] = record_df["record"].apply(lambda x: len(x.get_fields()))
+    record_df["publication_date"] = record_df["record"].apply(lambda x: get_pub_date(x))
 
-    return df
+    return record_df
 
 
 def update_marc_table(table, df, highlight_button, match_exists):
